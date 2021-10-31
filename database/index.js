f58e9acc3d3d2f7865245dde2d60804ef8c88414
Fabricio Kullolli <fabriciokullolli@gmail.com>
2021-10-31 20:40:32 +0100

    removed database config, prefer using sequalize db var

--- database/index.js
+++
@@ -1,8 +0,0 @@
-const {Sequalize} = require('sequelize')
-
-const sequalize = new Sequalize('users', process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD,  {
-  dialect: 'mysql',
-  dialectOptions: {}
-})
-
-export default sequalize
