96138440464c7de59be0d4f03680fa71d45f6f84
Niko <nikohysa92@gmail.com>
2021-11-01 19:21:44 +0100

    Added delete to users

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ const app = express()
 const PORT = 3000
 
 app.get('/', async (req, res) => {
-  //res.send(sequelize)
-  res.send(await sequelize.models.User.findAll())
+    //res.send(sequelize)
+    res.send(await sequelize.models.User.findAll())
+})
+
+app.delete('/:id', async (req, res) => {
+    const userId = req.params.id
+    const deleted = await sequelize.models.User.destroy({where: {id: userId}})
+    if (!deleted)
+        res.status(404).end()
+    res.status(204).end()
 })
 
 app.listen(PORT, () => {
-     console.log(`Example app listening at http://localhost:${PORT}`)
+    console.log(`Example app listening at http://localhost:${PORT}`)
 })
