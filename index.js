7da3a3c9ed2093aaba52a2730268df246dcb9737
Fabricio Kullolli <fabriciokullolli@gmail.com>
2021-11-03 19:15:03 +0100

    Added feature to get a user with an ID

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.get('/', async (req, res) => {
   res.send(await sequelize.models.User.findAll())
 })
 
+app.get('/:id', async (req, res) => {
+  const userId = req.params.id
+  const user = await sequelize.models.User.findOne({ where: {id: userId} })
+  res.send(user)
+})
+
 app.listen(PORT, () => {
      console.log(`Example app listening at http://localhost:${PORT}`)
 })
